4e32f43420dbcd09297d394b4a1e6fc0a214fc94
Ethan Kline <emkline@gmail.com>
2023-03-29 16:54:00 -0500

    adding a album servlet 4

--- src/main/webapp/scripts/spotify.js
+++ src/main/webapp/scripts/spotify.js
@@ -3,11 +3,12 @@ window.onSpotifyIframeApiReady = (IFrameAPI) => {
     let element = document.getElementById('embed-iframe');
     let u = document.getElementById("u").innerHTML
     let  l = u.toString()
+    let test = 'spotify:track:3D9TYPbNo1nnTwE4eLKxnJ'
     console.log(l)
     //console.log(u)
     //console.log(document.getElementById("u").innerHTML)
     let options = {
-        uri: 'spotify:track:3D9TYPbNo1nnTwE4eLKxnJ'
+        uri: test
     };
     let callback = (EmbedController) => {};
     IFrameAPI.createController(element, options, callback);
